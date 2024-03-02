2528d9d7546522faeceb32e1dae6d1bdef03b946
Koustubha068 <KoustuK@gmail.com>
2024-03-01 21:30:24 -0500

    made responsive

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function myFunction() {
+    let userInput = document.querySelector("#userInput")
+    let message = document.querySelector("#message")
+    message.innerHTML ="Welcome to the game, " + userInput.value +"!"
 
+
+}
+
+const image1 = 'papers-removebg-preview.png'
+const image2 = 'Rock-removebg-preview (1).png'
+const image3 = 'scissors-removebg-preview (1).png'
+
+function button (image1){
+    document.getElementById('player').src =image1
+
+}
 
 
